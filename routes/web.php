9ea0ec0f227de765866790b4d77fe20496bf0995
EldyLaksana <eldylaksana@gmail.com>
2024-11-14 14:23:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BackendController;
+use App\Http\Controllers\Carousel2Controller;
 use App\Http\Controllers\CarouselController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontendController;
@@ -80,4 +81,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('/dashboard/majelis', MajelisController::class);
 
     Route::resource('/dashboard/carousel', CarouselController::class);
+
+    Route::resource('/dashboard/carousel2', Carousel2Controller::class);
 });
