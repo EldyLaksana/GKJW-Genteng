41879d55f387570d529e76345d3f4ef65e1bdff3
EldyLaksana <eldylaksana@gmail.com>
2024-12-09 10:22:52 +0700

    Update SitemapController.php

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\KabarJemaat;
+use App\Models\Kategori;
 use App\Models\Renungan;
 use Illuminate\Http\Request;
 
@@ -14,8 +15,11 @@ class SitemapController extends Controller
         $renungans = Renungan::where('status_publikasi', 'Sekarang')->get();
         $kabarJemaats = KabarJemaat::where('status_publikasi', 'Sekarang')->get();
 
+        // Ambil semua kategori
+        $categories = Kategori::all();
+
         // Kembalikan tampilan sitemap dengan header XML
-        return response()->view('sitemap', compact('renungans', 'kabarJemaats'))
+        return response()->view('sitemap', compact('renungans', 'kabarJemaats', 'categories'))
             ->header('Content-Type', 'application/xml');
     }
 }
