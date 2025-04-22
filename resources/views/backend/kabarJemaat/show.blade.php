919f599c10a02ef7019d9f609548bab732ce747d
EldyLaksana <eldylaksana@gmail.com>
2025-04-22 09:21:08 +0700

    Update show.blade.php

--- resources/views/backend/kabarJemaat/show.blade.php
+++ resources/views/backend/kabarJemaat/show.blade.php
@@ -33,6 +33,29 @@
 
                     <p>{!! $kabarJemaat->isi !!}</p>
 
+                    @if ($carouselImages->count() > 0)
+                        <div id="carouselExample" class="carousel slide my-4" data-bs-ride="carousel">
+                            <div class="carousel-inner">
+                                @foreach ($carouselImages as $index => $image)
+                                    <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                                        <img src="{{ asset('storage/' . $image->gambar) }}" class="d-block w-100"
+                                            alt="Gambar Carousel">
+                                    </div>
+                                @endforeach
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    @endif
+
                     @if ($kabarJemaat->embed)
                         <div class="ratio ratio-16x9">
                             {!! $kabarJemaat->embed !!}
